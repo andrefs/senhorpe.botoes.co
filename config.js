0beffcd8e4ef4e5efe2a11e8f9ea4dabb7b46b1e
André Santos <andrefs@cpan.org>
2015-07-13 23:59:38 +0100

    updated ids

---
+++ config.js
@@ -0,0 +1,30 @@
+exports.config = {
+    project:{
+        title         : 'Botões Senhor Pé',
+        description   : 'As melhores frases de Senhor Pé!',
+        url           : 'http://bussaco.botoes.co',
+        external_link : 'http://www.facebook.com/brunoaleixo',
+        facebook: {
+            id: "1016048086"
+        },
+        google: {
+            id: 'UA-65104930-1'
+        },
+    },
+    partials: {
+        footer: '<a href="mailto:andrefs@andrefs.com">andré santos, joão ferreira <i class="fa fa-envelope"></i></a> , baseado em <a href="http://www.facebook.com/brunoaleixo">bruno aleixo <i class="fa fa-external-link"></i></a>'
+    },
+    manifest : {
+        version: 'v1',
+        filePath: './',
+        fileName: 'manifest.json'
+    },
+    sourcesPath       : './sources',
+    soundsPath        : './sounds',
+    tmpPath           : './tmp',
+    defaultClipMargin : '2',
+    soundEditCommand  : 'audacity',
+    server: {
+        port: 10117
+    }
+};
