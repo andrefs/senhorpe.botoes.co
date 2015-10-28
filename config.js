c31d6ac4da4a55259aaec89fbf3244d1d001c876
André Santos <andrefs@cpan.org>
2015-10-28 21:08:48 +0000

    Updates template

--- config.js
+++ config.js
@@ -24,6 +24,21 @@ exports.config = {
     tmpPath           : './tmp',
     defaultClipMargin : '2',
     soundEditCommand  : 'audacity',
+    toplinks: [
+        {
+            link: 'http://aleixo.botoes.co',
+            title: 'Aleixo'
+        },{
+            link: 'http://renato.botoes.co',
+            title: 'Renato'
+        },{
+            link: 'http://bussaco.botoes.co',
+            title: 'Bussaco'
+        },{
+            link: 'http://beatriz.botoes.co',
+            title: 'Beatriz'
+        }
+    ],
     server: {
         port: 10117
     }
